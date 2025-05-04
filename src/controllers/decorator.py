d278fa4bbb48a0da4ac310725d0835ccc6acf27a
Jvan Kocherhans <kocherhansjvan@gmail.com>
2025-05-04 14:21:05 +0200

    added session based accesscontrol and fixed docker issue

--- src/controllers/decorator.py
+++ src/controllers/decorator.py
@@ -1,4 +1,4 @@
-from flask import redirect, flash
+from flask import redirect, flash, request
 from flask_login import current_user
 from functools import wraps
 
@@ -6,14 +6,14 @@ def requires_access_level(access_level):
     def decorator(f):
         @wraps(f)
         def decorated_function(*args, **kwargs):
-            if not current_user.is_authenticated: #the user is not logged in
-                return redirect('/')
+            if not current_user.is_authenticated:
+                return redirect('/login')
 
             #user = User.query.filter_by(id=current_user.id).first()
 
             if not current_user.allowed(access_level):
-                flash('You do not have access to this resource.', 'danger')
-                return redirect('/')
+                flash('You do not have access to this resource.')
+                return redirect(request.referrer)
             return f(*args, **kwargs)
         return decorated_function
     return decorator
--- src/controllers/user.py
+++ src/controllers/user.py
@@ -4,49 +4,52 @@ from flask_login import UserMixin, login_user, LoginManager, login_required, log
 from ldap3 import Server, Connection, ALL, SUBTREE
 from ldap3.core.exceptions import LDAPException, LDAPBindError
 
-from flask import render_template, request, redirect, url_for, flash
-from authentication import *
+from flask import render_template, request, redirect, url_for, flash, session
 from forms.LoginForm import *
 from flask_cors import CORS, cross_origin
 from flask.sessions import SecureCookieSessionInterface
 from itsdangerous import URLSafeTimedSerializer
 
-users_blueprint = Blueprint('users_blueprint', __name__)
+from controllers.decorator import requires_access_level
+from model.ACCESS import ACCESS
 
-@users_blueprint.route("/login", methods=["GET", "POST"])
-def index():
+def create_user_blueprint(ldapmanager_conn):
 
-    # initiate the form..
-    form = LoginValidation()
+    user_blueprint = Blueprint('users_blueprint', __name__)
 
-    if request.method in ('POST') :
-        login_user = form.user_name.data
-        login_password = form.user_password.data
+    @user_blueprint.route("/login", methods=["GET", "POST"])
+    def login():
+        # initiate the form..
+        form = LoginValidation()
 
-        login_msg = global_ldap_authentication(login_user, login_password)
+        if request.method in ('POST') :
+            ldap_login_user = form.user_name.data
+            ldap_login_password = form.user_password.data
 
-        # validate the connection
-        if login_msg == "Success":
-            success_message = f"*** Authentication Success "
-            return render_template('search.html', success_message=success_message)
+            ldap_user = ldapmanager_conn.authentication(ldap_login_user, ldap_login_password)
 
-        else:
-            error_message = f"*** Authentication Failed - {login_msg}"
-            return render_template("error.html", error_message=str(error_message))
+            # validate the connection
+            if ldap_user:
+                login_user(ldap_user)
+                session['access'] = ldap_user.access
+                success_message = f"Authentication Success "
+                return redirect('/search')
 
-    return render_template('login.html', form=form)
+            else:
+                error_message = f"Authentication Failed"
+                return render_template("error.html", error_message=str(error_message))
 
-@users_blueprint.route("/profile", methods=["GET"])
-def profile():
-    return render_template('profile.html')
+        return render_template('login.html', form=form)
+    
+    @user_blueprint.route("/profile", methods=["GET"])
+    @requires_access_level(ACCESS['user'])
+    def profile():
+        return render_template('profile.html', current_user=ldapmanager_conn.get_user(current_user.id))
 
+    @user_blueprint.route("/logout", methods=["GET"])
+    @requires_access_level(ACCESS['user'])
+    def logout():    
+        logout_user()
+        return redirect("/login")
 
-# @users_blueprint.route("/dashboard", methods=["GET", "POST"])
-# @requires_access_level(ACCESS['guest'])
-# def dashboard():
-#     pass
-
-# @users_blueprint.route("/logout", methods=["GET", "POST"])
-# @login_required
-# def logout():
-#     pass
+    return user_blueprint
